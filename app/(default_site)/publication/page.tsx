623cd5e0784d2b3d363a206d59a69daaf5414e6f
faikit <huijie.wang@hotmail.com>
2025-03-28 23:29:43 +0800

    pub

--- app/(default_site)/publication/page.tsx
+++ app/(default_site)/publication/page.tsx
@@ -22,6 +22,7 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import { pubblications } from "./paper"
 
 
 
@@ -53,43 +54,6 @@ const categories: { c1: string; c2s: string[]; }[] = [
             "Ping Lup",
             "Hongyang Li",
         ]
-    },
-]
-
-
-
-const pubblications: { title: string; authors: string; proceedings: string; links: { website: string; url: string; }[]; keys: string[]; }[] = [
-    {
-        title: "Planning-oriented autonomous driving",
-        authors: "Yihan Hu, Jiazhi Yang, Li Chen, Keyu Li, Chonghao Sima, Xizhou Zhu, Siqi Chai, Senyao Du, Tianwei Lin, Wenhai Wang, Lewei Lu, Xiaosong Jia, Qiang Liu, Jifeng Dai, Yu Qiao, Hongyang Li",
-        proceedings: "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR), 2023",
-        links: [
-            {
-                website: "proceeding",
-                url: "https://openaccess.thecvf.com/content/CVPR2023/html/Hu_Planning-Oriented_Autonomous_Driving_CVPR_2023_paper.html",
-            },
-        ],
-        keys :[
-            "key1", "key2",
-        ],
-    },
-    {
-        title: "Topic",
-        authors: "scadj fijewaofcjw dw",
-        proceedings: "daf avrea ",
-        links: [
-            {
-                website: "xxx",
-                url: "https://opendrivelab.com",
-            },
-            {
-                website: "xdwxx",
-                url: "https://opendrivelab.com",
-            },
-        ],
-        keys :[
-            "key1", "key2", "key22",
-        ],
     },
 ]
 
