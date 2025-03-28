9df3cdb32418eb06a4d03c65a784b56284c44fe0
faikit <huijie.wang@hotmail.com>
2025-03-28 13:43:04 +0800

    .

--- app/(default_site)/publication/page.tsx
+++ app/(default_site)/publication/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
                     </h1>
                 </div>
             </div>
-
+        
 
 
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
@@ -46,6 +46,9 @@ export default function Home() {
                         </span> 
                     </a>
                 </h2>
+                <div className="w-full mt-20">
+                    some works with charming figures
+                </div>
             </div>
 
 
@@ -53,32 +56,48 @@ export default function Home() {
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
                 <Separator className="max-w-7xl mt-20"/>
             </div>
-            
+
 
 
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
+                
+                <h2 className="w-full max-w-7xl mt-20 text-6xl font-bold"> 
+                    <a href="#galary" className="scroll-mt-20 group flex items-center" id="galary">
+                        Galary
+                        <span className="ml-6 hidden group-hover:inline-block size-10 text-soft-black">
+                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
+                                <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
+                            </svg>
+                        </span> 
+                    </a>
+                </h2>
+
+
+
                 <div className="w-full max-w-7xl mt-20 flex flex-col md:flex-row">
 
 
-
-                    <div >
-                        <h2 className="text-xl font-bold">
-                            Sort by
-                        </h2>
-                        <RadioGroup defaultValue="topic" className="mt-6 flex flex-col gap-6">
-                            <div className="flex items-center space-x-3">
-                                <RadioGroupItem value="topic" id="r1" />
-                                <Label htmlFor="r1">Topic</Label>
-                            </div>
-                            <div className="flex items-center space-x-3">
-                                <RadioGroupItem value="year" id="r2" />
-                                <Label htmlFor="r2">Year</Label>
-                            </div>
-                            <div className="flex items-center space-x-3">
-                                <RadioGroupItem value="pi" id="r3" />
-                                <Label htmlFor="r3">PI</Label>
-                            </div>
-                        </RadioGroup>
+                    <div>
+                        <div className="sticky top-32">
+                            <h2 className="text-xl font-bold">
+                                Sort by
+                            </h2>
+                            <RadioGroup defaultValue="topic" className="mt-6 flex flex-col gap-6">
+                                <div className="flex items-center space-x-3">
+                                    <RadioGroupItem value="topic" id="r1" />
+                                    <Label htmlFor="r1">Topic</Label>
+                                </div>
+                                <div className="flex items-center space-x-3">
+                                    <RadioGroupItem value="year" id="r2" />
+                                    <Label htmlFor="r2">Year</Label>
+                                </div>
+                                <div className="flex items-center space-x-3">
+                                    <RadioGroupItem value="pi" id="r3" />
+                                    <Label htmlFor="r3">PI</Label>
+                                </div>
+                            </RadioGroup>
+                        </div>
                     </div>
 
 
