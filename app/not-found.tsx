1ce59c5103a279edd4bdbbad991f00818e37adc5
faikit <huijie.wang@hotmail.com>
2025-03-28 10:21:05 +0800

    404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,19 +11,28 @@ import Link from 'next/link'
 
 
 
+import { FadeIn } from "@/components/animation/fade-in"
+
+
+
+
 export default function NotFound() {
     return (
-        <main className='h-svh w-full flex flex-col justify-center items-center'>
-            <div className="flex flex-col gap-6">
-                <h1 className="font-bold text-6xl">
-                    404
-                </h1>
-                <h1 className="font-bold text-6xl">
-                    Not Found
-                </h1>
-                <Link href="/" className='hover:underline'>
-                    return home
-                </Link>
+        <main>
+            <div className="w-full h-svh flex flex-row justify-center items-center pl-6 pr-6 bg-gradient-to-br from-indigo-300 via-yellow-400 to-pink-500 bg-fixed">
+                <FadeIn>
+                    <div className="w-full max-w-7xl flex flex-col gap-6 select-none">
+                        <h1 className="font-bold text-3xl sm:text-6xl text-white">
+                            404
+                        </h1>
+                        <h1 className="font-bold text-3xl sm:text-6xl text-white">
+                            Not Found
+                        </h1>
+                        <Link href="/" className='font-bold text-white hover:underline'>
+                            return home
+                        </Link>
+                    </div>
+                </FadeIn>
             </div>
         </main>
     )
