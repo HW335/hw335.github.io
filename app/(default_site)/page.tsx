b8a7fb11fadf658fe8dc0d9878ad08de349cf5f3
faikit <huijie.wang@hotmail.com>
2025-03-29 16:21:45 +0800

    .

--- app/(default_site)/page.tsx
+++ app/(default_site)/page.tsx
@@ -11,9 +11,16 @@ import Image from 'next/image'
 
 
 
+import { FadeIn } from "@/components/animation/fade-in"
+
+
+
 export default function Home() {
     return (
         <main className='h-svh w-full flex flex-col justify-center items-center'>
+
+
+
             <div className="flex flex-col gap-6">
                 <Image src="/logos/jc_stem_lab.png" width={50} height={50} alt="e"/>
                 <h1 className="font-bold text-6xl">
@@ -23,6 +30,26 @@ export default function Home() {
                     LAB
                 </h1>
             </div>
+
+
+
+            <FadeIn>
+                <div className="w-full pl-6 pr-6 flex flex-col items-center">
+                    <h2 className="w-full max-w-7xl mt-20 text-6xl font-bold"> 
+                        <a href="#selected" className="scroll-mt-20 group flex items-center" id="selected">
+                            Selected Works
+                            <span className="ml-6 hidden group-hover:inline-block size-10 text-soft-black">
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                    <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
+                                    <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
+                                </svg>
+                            </span> 
+                        </a>
+                    </h2>
+                </div>
+            </FadeIn>
+
+
                 
         </main>
     );
