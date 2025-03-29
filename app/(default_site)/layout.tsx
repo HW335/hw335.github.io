05748d0ea50afc779488dfffdd15149a2d8ad54b
faikit <wongfaikit0327@gmail.com>
2025-03-29 23:55:01 +0800

    .

--- app/(default_site)/layout.tsx
+++ app/(default_site)/layout.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Image from 'next/image'
+import { Separator } from "@/components/ui/separator"
 
 
 
@@ -8,6 +9,14 @@ import "../globals.css";
 
 
 import { FadeIn } from "@/components/animation/fade-in"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 
 
 
@@ -106,7 +115,7 @@ export default function RootLayout({
                     <footer className="w-full mb-20 flex flex-col gab-10">
                         <div className="w-full pl-6 pr-6 flex flex-col items-center 3xl:hidden">
                             <div className="w-full max-w-7xl mt-20 flex gap-10 items-center">
-                                <span className="font-bold text-4xl">
+                                <span className="font-bold text-4xl select-none">
                                     MMLab | Multimedia Laboratory
                                 </span>
                             </div>
@@ -115,7 +124,7 @@ export default function RootLayout({
                         <div className="w-full pl-6 pr-6 flex justify-center 3xl:justify-evenly">
                             <div className="w-48 hidden 3xl:block overflow-hidden">
                                 <div className="w-full max-w-7xl mt-20 flex flex-col gap-3">
-                                    <span className="font-bold text-4xl">
+                                    <span className="font-bold text-4xl select-none">
                                         MMLab
                                     </span>
                                 </div>
@@ -123,12 +132,12 @@ export default function RootLayout({
                             <div className="w-full max-w-7xl mt-20 grid gap-10 grid-cols-2 lg:grid-cols-4">
                                 {footers.map((footer) => (
                                     <div key={footer.title} className="flex flex-col gap-3">
-                                        <h3 className="font-bold text-xl mb-3">
+                                        <h3 className="font-bold text-xl mb-3 select-none">
                                             {footer.title}
                                         </h3>
                                         {footer.links.map((f) => (
                                             <div key={f.subtitle}>
-                                                <a key={f.subtitle} href={f.href} target={f.target} className="hover:underline">
+                                                <a key={f.subtitle} href={f.href} target={f.target} className="hover:underline select-none">
                                                     {f.subtitle}
                                                 </a>
                                             </div>
@@ -139,11 +148,43 @@ export default function RootLayout({
                             <div className="w-48 hidden 3xl:block overflow-hidden"></div>
                         </div>
                         {/* --- */}
-                        <div className="w-full pl-6 pr-6 flex flex-col items-center">
-                            <div className="w-full max-w-7xl mt-20 flex items-center">
-                                <span className="select-none">
-                                    © 2025 All Rights Reserved
-                                </span>
+                        <div className="w-full pl-6 pr-6 flex flex-row justify-center mt-20">
+                            <div className="max-w-7xl w-full flex flex-row justify-between">
+                                <div className="flex-1 flex flex-col justify-start">
+                                    <span className="select-none">
+                                        MMLab © 2001 - 2025
+                                    </span>
+                                    <Dialog>
+                                        <DialogTrigger asChild>
+                                            <span className="hover:underline cursor-pointer select-none">Site Credits</span>
+                                        </DialogTrigger>
+                                        <DialogContent className="sm:max-w-3xl p-6">
+                                            <DialogHeader className="p-3">
+                                                <DialogTitle>
+                                                    Site Credits
+                                                </DialogTitle>
+                                                <div className="w-full flex flex-col items-center">
+                                                    <Separator className="mt-3"/>
+                                                </div>
+                                                <DialogDescription className="mt-3">
+                                                    <span>
+                                                        Thanks to the following site developers and all lab members for contributions.
+                                                    </span>
+                                                    <ul className="mt-3 ml-6 space-y-3 list-outside list-disc">
+                                                        <li><a href="https://faikit.github.io/" target="_blank" className="hover:underline">Huijie Wang</a></li>
+                                                    </ul>
+                                                </DialogDescription>
+                                            </DialogHeader>
+                                            {/* <DialogFooter className="sm:justify-start">
+                                            <DialogClose asChild>
+                                                <Button type="button" variant="secondary">
+                                                Close
+                                                </Button>
+                                            </DialogClose>
+                                            </DialogFooter> */}
+                                        </DialogContent>
+                                    </Dialog>
+                                </div>
                             </div>
                         </div>
                     </footer>
