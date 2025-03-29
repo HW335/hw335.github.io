a80e8840acbd42cc1c15d9b030f9045c655be03a
faikit <wongfaikit0327@gmail.com>
2025-03-30 00:16:41 +0800

    .

--- app/(default_site)/layout.tsx
+++ app/(default_site)/layout.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import Image from 'next/image'
 import { Separator } from "@/components/ui/separator"
 
 
@@ -96,6 +95,15 @@ const footers: { title: string; links: { subtitle: string; href: string; target:
 
 
 
+const icons: { link: string; icon: string; }[] = [
+    {
+        link: "https://x.com/",
+        icon: '<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z"/></svg>',
+    },
+]
+
+
+
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -115,7 +123,7 @@ export default function RootLayout({
                     <footer className="w-full mb-20 flex flex-col gab-10">
                         <div className="w-full pl-6 pr-6 flex flex-col items-center 3xl:hidden">
                             <div className="w-full max-w-7xl mt-20 flex gap-10 items-center">
-                                <span className="font-bold text-4xl select-none">
+                                <span className="font-bold text-3xl select-none">
                                     MMLab | Multimedia Laboratory
                                 </span>
                             </div>
@@ -123,8 +131,8 @@ export default function RootLayout({
                         {/* full width */}
                         <div className="w-full pl-6 pr-6 flex justify-center 3xl:justify-evenly">
                             <div className="w-48 hidden 3xl:block overflow-hidden">
-                                <div className="w-full max-w-7xl mt-20 flex flex-col gap-3">
-                                    <span className="font-bold text-4xl select-none">
+                                <div className="w-full mt-20 flex flex-col gap-3">
+                                    <span className="font-bold text-3xl select-none">
                                         MMLab
                                     </span>
                                 </div>
@@ -145,7 +153,17 @@ export default function RootLayout({
                                     </div>
                                 ))}
                             </div>
-                            <div className="w-48 hidden 3xl:block overflow-hidden"></div>
+                            <div className="w-48 hidden 3xl:block overflow-hidden">
+                                <div className="w-full mt-20 flex flex-row flex-wrap gap-6 select-none">
+
+                                    <a href="https://x.com/" target="_blank" className="hover:underline select-none hover:opacity-30">
+                                        <svg className="size-8 text-foreground" aria-hidden="true" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z"/>
+                                        </svg>
+                                    </a>
+
+                                </div>
+                            </div>
                         </div>
                         {/* --- */}
                         <div className="w-full pl-6 pr-6 flex flex-row justify-center mt-20">
@@ -153,6 +171,9 @@ export default function RootLayout({
                                 <div className="flex-1 flex flex-col justify-start">
                                     <span className="select-none">
                                         MMLab © 2001 - 2025
+                                    </span>
+                                    <span className="select-none">
+                                        All Rights Reserved
                                     </span>
                                     <Dialog>
                                         <DialogTrigger asChild>
