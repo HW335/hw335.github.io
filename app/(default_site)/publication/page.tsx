49f38b24dc20522fd1f7ae1ff3d656f0925ce0bf
faikit <wongfaikit0327@gmail.com>
2025-03-31 20:31:14 +0800

    .

--- app/(default_site)/publication/page.tsx
+++ app/(default_site)/publication/page.tsx
@@ -43,10 +43,10 @@ export default function Home() {
 
 
             {/* Landing */}
-            <div className="w-full h-svh flex flex-row justify-center items-center pl-6 pr-6 bg-gradient-to-br from-[#b5a774] via-[#e59c2e] to-[#D71440] bg-fixed">
-                <div className="w-full max-w-7xl flex flex-row justify-start">
-                    <h1 className="font-bold text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-white">
-                        <FadeIn>Publication</FadeIn>
+            <div className="w-full h-64 flex flex-row justify-center items-end pl-6 pr-6 bg-gradient-to-br from-[#b5a774] via-[#e59c2e] to-[#D71440] bg-fixed">
+                <div className="w-full max-w-7xl flex flex-row justify-start pb-10">
+                    <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white">
+                        <FadeIn>All Publications</FadeIn>
                     </h1>
                 </div>
             </div>
@@ -56,18 +56,6 @@ export default function Home() {
             <FadeIn>
                 <div className="w-full pl-6 pr-6 flex flex-col items-center">
                     
-                    <h2 className="w-full max-w-7xl mt-20 text-6xl font-bold"> 
-                        <Link href="#gallery" className="scroll-mt-20 group flex items-center" id="gallery">
-                            Gallery
-                            <span className="ml-6 hidden group-hover:inline-block size-10 text-foreground">
-                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                                    <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
-                                    <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
-                                </svg>
-                            </span> 
-                        </Link>
-                    </h2>
-
 
 
                     <div className="w-full max-w-7xl mt-20">
